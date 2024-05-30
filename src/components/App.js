49848cdd354f16a3cd0020060382c31e120b1764
Allan Kipkorir <allantuix@gmail.com>
2024-05-30 22:02:22 +0300

    Added Keypad functionality and App.js to render the work

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Keypad from './Keypad'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App</h1>
+      <Keypad />
+    </div>
+  );
+}
+
+export default App;
